7eeaf87607b5053d790f3350b92e8be8421efd48
Chepa1 <89663997+Chepa1@users.noreply.github.com>
2025-04-08 16:29:13 -0400

    implementa validateForm (sin validar fotos)

--- js/validation.js
+++ js/validation.js
@@ -1,9 +1,11 @@
 const validateRegion = (region) => {
     if (!region) return false;
+    return true;
 }
 
 const validateComuna = (comuna) => {
     if (!comuna) return false;
+    return true;
 }
 
 const validateName = (name) => {
@@ -33,10 +35,14 @@ const validateTel = (tel) => {
 
 const validateFormaContacto = (forma) => {
     if (forma != "") {
-        let validLength = 4 <= forma.trim().length && forma.trim().length <= 50;
-        return validLength;
+        let urlContacto = document.getElementById("url-contacto");
+        if (urlContacto.value != "") {
+            let validLength = 4 <= urlContacto.value.trim().length && urlContacto.value.trim().length <= 50;
+            return validLength;
+        }
+        return true;
     }
-    return true;
+    return false;
 };
 
 const validateFechaIni = (fecha) => {
@@ -73,6 +79,7 @@ const validateTema = (tema) => {
 
         return lengthValid;
     }
+    return true;
 };
 
 const validateFotos = (foto) => {
@@ -80,16 +87,80 @@ const validateFotos = (foto) => {
 };
 
 const validateForm = () => {
-    let myForm = document.getElementById("form-add");
+    let myForm = document.forms["form-donde"];
+    let email = myForm["email"].value;
+    let phoneNumber = myForm["phone"].value;
+    let name = myForm["nombre"].value;
+    let region = myForm["select-region"].value;
+    let comuna = myForm["select-comuna"].value;
+    let formaContacto = myForm["select-forma-contacto"].value;
+    let fechaIni = myForm["tiempo-inicio"].value;
+    let fechaTer = myForm["tiempo-termino"].value;
+    let tema = myForm["select-tema"].value;
 
+    let invalidInputs = [];
     let isValid = true;
+    const setInvalidInput = (inputName) => {
+        invalidInputs.push(inputName);
+        isValid &&= false;
+    };
 
+    // lógica de validación
+    if (!validateRegion(region)) {
+        setInvalidInput("Región");
+    }
+    if (!validateComuna(comuna)) {
+        setInvalidInput("Comuna");
+    }
+    if (!validateName(name)) {
+        setInvalidInput("Nombre");
+    }
+    if (!validateEmail(email)) {
+        setInvalidInput("Email");
+    }
+    if (!validateTel(phoneNumber)) {
+        setInvalidInput("Número");
+    }
+    if (!validateFormaContacto(formaContacto)) {
+        setInvalidInput("Forma de contacto");
+    }
+    if (!validateFechaIni(fechaIni)) {
+        setInvalidInput("Fecha de inicio");
+    }
+    if (!validateFechaTer(fechaTer, fechaIni)) {
+        setInvalidInput("Fecha de Ter");
+    }
+    if (!validateTema(tema)) {
+        setInvalidInput("Tema");
+    }
+
+    let validationBox = document.getElementById("val-box");
+    let validationMessageElem = document.getElementById("val-msg");
+    let validationListElem = document.getElementById("val-list");
     let confirmationBox = document.getElementById("confirmation-box");
 
     if (!isValid) {
+        validationListElem.textContent = "";
+        
+        for (input of invalidInputs) {
+            let listElement = document.createElement("li");
+            listElement.innerText = input;
+            validationListElem.append(listElement);
+        }
+        // establecer val-msg
+        validationMessageElem.innerText = "Los siguientes campos son inválidos:";
+
+        // aplicar estilos de error
+        validationBox.style.backgroundColor = "#ffdddd";
+        validationBox.style.borderLeftColor = "#f44336";
+
+        // hacer visible el mensaje de validación
+        validationBox.hidden = false;
     }
     else {
-        myForm.style.display = "none";
+        let formDiv = document.getElementById("form-add");
+        validationBox.hidden = true;
+        formDiv.style.display = "none";
         confirmationBox.style.display = "block";
     }
 }
