ff39d8bc662db42246487b535e725afaf9bb722e
Chepa1 <89663997+Chepa1@users.noreply.github.com>
2025-05-16 23:15:25 -0400

    agrega funciones a db.py

--- flask_app/database/db.py
+++ flask_app/database/db.py
@@ -12,6 +12,7 @@ engine = create_engine(DATABASE_URL, echo=False)
 SessionLocal = sessionmaker(bind=engine)
 Base = declarative_base()
 
+# modelos
 class Region(Base):
     __tablename__ = "region"
     id = Column(Integer, primary_key=True, autoincrement=True)
@@ -65,3 +66,48 @@ class ActividadTema(Base):
     glosa_otro = Column(String(15))
     actividad_id = Column(Integer, ForeignKey("actividad.id"), nullable=False)
     actividad = relationship("Actividad", back_populates="temas")
+
+# funciones
+def list_actividades(limit=5):
+    session = SessionLocal()
+    actividades = session.query(Actividad).order_by(Actividad.dia_hora_inicio.desc()).limit(limit).all()
+    session.close()
+    return actividades
+
+def get_actividad_by_id(actividad_id):
+    session = SessionLocal()
+    actividad = session.query(Actividad).get(actividad_id)
+    session.close()
+    return actividad
+
+def create_actividad(data):
+    session = SessionLocal()
+    nueva = Actividad(**data)
+    session.add(nueva)
+    session.commit()
+    session.refresh(nueva)
+    session.close()
+
+
+def add_tema(actividad_id, tema, glosa_otro=None):
+    session = SessionLocal()
+    at = ActividadTema(actividad_id=actividad_id, tema=tema, glosa_otro=glosa_otro)
+    session.add(at)
+    session.commit()
+    session.close()
+
+
+def add_contacto(actividad_id, nombre, identificador):
+    session = SessionLocal()
+    cp = ContactarPor(actividad_id=actividad_id, nombre=nombre, identificador=identificador)
+    session.add(cp)
+    session.commit()
+    session.close()
+
+
+def add_foto(actividad_id, ruta_archivo, nombre_archivo):
+    session = SessionLocal()
+    f = Foto(actividad_id=actividad_id, ruta_archivo=ruta_archivo, nombre_archivo=nombre_archivo)
+    session.add(f)
+    session.commit()
+    session.close()
